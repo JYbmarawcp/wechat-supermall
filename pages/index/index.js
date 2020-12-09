7e620be3fb00af9ee51f9e70285244f82ffd6a52
JYbmarawcp <fjh2333@gmail.com>
2020-12-09 16:19:47 +0800

    首页布局完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,72 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
+import { request } from "../../request/index"
+//Page Object
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    bannerList: [], // 轮播图数组
+    cateList: [], // 导航
+    floorList: [], // 楼层
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+  // 页面开始加载就会触发
+  onLoad: function(options) {
+    // 发送请求 优化的手段为promise
+    this.getBannerList()
+    this.getCateList()
+    this.getFloorList()
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
+  getBannerList() {
+    request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
+    }).then(result => {
       this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
+        bannerList: result.data.message
       })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
     })
+  },
+  getCateList() {
+    request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/catitems',
+    }).then(result => {
+      this.setData({
+        cateList: result.data.message
+      })
+    })
+  },
+  getFloorList() {
+    request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/floordata',
+    }).then(result => {
+      this.setData({
+        floorList: result.data.message
+      })
+    })
+  },
+  onReady: function() {
+    
+  },
+  onShow: function() {
+    
+  },
+  onHide: function() {
+
+  },
+  onUnload: function() {
+
+  },
+  onPullDownRefresh: function() {
+
+  },
+  onReachBottom: function() {
+
+  },
+  onShareAppMessage: function() {
+
+  },
+  onPageScroll: function() {
+
+  },
+  //item(index,pagePath,text)
+  onTabItemTap:function(item) {
+
   }
-})
+});
+  
